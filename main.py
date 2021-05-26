7baa40587f7fbc613c8ef30db837c7684b3d6a66
Mehul <79636200+Mehul7777777@users.noreply.github.com>
2021-05-26 12:45:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def eng_to_morse_conversion(message):
+    converted_text = ""
+    for letter in message:
+        if letter != " ":
+            morse_value_for_letter = MORSE_CODE_DICT[letter]
+            converted_text += morse_value_for_letter
+        else:
+            converted_text += " "
+    print(f"The converted text for this above message is: {converted_text}")
+
+
+message = input("Enter the message to convert it into morse code: ")
+eng_to_morse_conversion(message.upper())
+
+
+
+
+
+
+
+
+
